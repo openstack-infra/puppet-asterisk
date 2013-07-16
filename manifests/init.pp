064f5d570801d74b9d3944613e8fd39d1248cb29
Russell Bryant <rbryant@pbx.openstack.org>
2013-07-16 22:38:14 +0000

    Install Asterisk
    Add the necessary bits to get Asterisk installed on the pbx node.
    This is using Asterisk 11 from Digium's repo.
    
    Change-Id: I200789c7ee7fc1fb2e0779a38db2ea35ead998ae

---
+++ manifests/init.pp
@@ -0,0 +1,39 @@
+# Copyright 2013 Red Hat, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+#
+# Class to configure asterisk on a CentOS node.
+#
+# == Class: asterisk
+class asterisk (
+) {
+  yumrepo { 'asteriskcurrent':
+    baseurl  => 'http://packages.asterisk.org/centos/$releasever/current/$basearch/',
+    descr    => 'Asterisk supporting packages produced by Digium',
+    enabled  => 1,
+    gpgcheck => 0,
+  }
+
+  yumrepo { 'asterisk11':
+    baseurl  => 'http://packages.asterisk.org/centos/$releasever/asterisk-11/$basearch/',
+    descr    => 'Asterisk packages produced by Digium',
+    enabled  => 1,
+    gpgcheck => 0,
+    require  => Yumrepo['asteriskcurrent'],
+  }
+
+  package { 'asterisk' :
+    ensure  => present,
+    require => Yumrepo['asterisk11'],
+  }
+}
