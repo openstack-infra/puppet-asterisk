cacc6fb490cde23a1c37ae8211b12da5c7617177
Colleen Murphy <colleen.murphy@suse.de>
2018-07-10 14:59:39 +0200

    Add beaker tests
    Add tests to ensure the asterisk class can be applied correctly and the
    service can start.
    
    Change-Id: I9714e040d6c73e7b0e8ce5d9fe91f2674214efa6

---
+++ spec/acceptance/basic_spec.rb
@@ -0,0 +1,27 @@
+require 'puppet-openstack_infra_spec_helper/spec_helper_acceptance'
+
+describe 'asterisk' :if => os[:family] == 'ubuntu' do
+
+  def pp_path
+    base_path = File.dirname(__FILE__)
+    File.join(base_path, 'fixtures')
+  end
+
+  def puppet_manifest
+    manifest_path = File.join(pp_path, 'default.pp')
+    File.read(manifest_path)
+  end
+
+  it 'should work with no errors' do
+    apply_manifest(puppet_manifest, catch_failures: true)
+  end
+
+  it 'should be idempotent' do
+    apply_manifest(puppet_manifest, catch_changes: true)
+  end
+
+  describe service('asterisk') do
+    it { should be_running }
+  end
+
+end
